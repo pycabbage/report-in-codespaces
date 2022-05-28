a18e29c397bf958fd3ca38c7bba39eff3b0d3ceb
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-05-29 03:01:29 +0900

    fix

--- .devcontainer/scripts/install_texlive.sh
+++ .devcontainer/scripts/install_texlive.sh
@@ -9,11 +9,24 @@ MOUNT_PATH=/media/texlive
 
 apt install aria2 kmod -y
 
+# Setup loop
+modprobe loop
+mknod /dev/loop0 -m0660 b 7 0
+mknod /dev/loop1 -m0660 b 7 1
+mknod /dev/loop2 -m0660 b 7 2
+mknod /dev/loop3 -m0660 b 7 3
+mknod /dev/loop4 -m0660 b 7 4
+mknod /dev/loop5 -m0660 b 7 5
+mknod /dev/loop6 -m0660 b 7 6
+mknod /dev/loop7 -m0660 b 7 7
+mknod /dev/loop8 -m0660 b 7 8
+mknod /dev/loop9 -m0660 b 7 9
+losetup -a
+
 # Download texlive ISO
 aria2c -x16 -s16 --file-allocation=none -c -d $(dirname $ISO_PATH) -o $(basename $ISO_PATH) --checksum=md5=$ISO_MD5 --checksum=sha-512=$ISO_SHA512 "$ISO_URL"
 # Mount ISO
 mkdir -p $MOUNT_PATH
-modprobe loop
 mount -o ro,loop,noauto -t iso9660 $ISO_PATH $MOUNT_PATH
 # Install texlive
 echo I | ${MOUNT_PATH}/install-tl -no-gui -lang ja
