5c0f58ae1edfb1a0bbbd94c187ab6e0915230463
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-05-29 04:17:08 +0900

    fix

--- .devcontainer/scripts/install_texlive.sh
+++ .devcontainer/scripts/install_texlive.sh
@@ -7,31 +7,20 @@ ISO_MD5=$(curl -kLs ${ISO_URL}.md5 | awk '{print $1}')
 ISO_SHA512=$(curl -kLs ${ISO_URL}.sha512 | awk '{print $1}')
 MOUNT_PATH=/media/texlive
 
-apt install aria2 kmod -y
-
-# Setup loop
-modprobe loop
-mknod /dev/loop0 -m0660 b 7 0
-mknod /dev/loop1 -m0660 b 7 1
-mknod /dev/loop2 -m0660 b 7 2
-mknod /dev/loop3 -m0660 b 7 3
-mknod /dev/loop4 -m0660 b 7 4
-mknod /dev/loop5 -m0660 b 7 5
-mknod /dev/loop6 -m0660 b 7 6
-mknod /dev/loop7 -m0660 b 7 7
-mknod /dev/loop8 -m0660 b 7 8
-mknod /dev/loop9 -m0660 b 7 9
-losetup -a
+apt install aria2 p7zip-full -y
 
 # Download texlive ISO
 aria2c -x16 -s16 --file-allocation=none -c -d $(dirname $ISO_PATH) -o $(basename $ISO_PATH) --checksum=md5=$ISO_MD5 --checksum=sha-512=$ISO_SHA512 "$ISO_URL"
 # Mount ISO
 mkdir -p $MOUNT_PATH
-mount -o ro,loop,noauto -t iso9660 $ISO_PATH $MOUNT_PATH
+# mount -o ro,loop,noauto -t iso9660 $ISO_PATH $MOUNT_PATH
+7z x $ISO_PATH -o${MOUNT_PATH}
 # Install texlive
+chmod +x ${MOUNT_PATH}/install-tl
 echo I | ${MOUNT_PATH}/install-tl -no-gui -lang ja
 # Unmount ISO
-umount -r $MOUNT_PATH
+# umount -r $MOUNT_PATH
+rm -fr ${MOUNT_PATH}
 # Remove ISO
 rm $ISO_PATH
 
