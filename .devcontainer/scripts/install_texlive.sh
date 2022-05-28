b8432440643bfbf38966730999e2abbc51cb6650
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-05-29 02:34:16 +0900

    fix!

--- .devcontainer/scripts/install_texlive.sh
+++ .devcontainer/scripts/install_texlive.sh
@@ -5,20 +5,15 @@ ISO_PATH=/tmp/texlive.iso
 echo Getting hashes...
 ISO_MD5=$(curl -kLs ${ISO_URL}.md5 | awk '{print $1}')
 ISO_SHA512=$(curl -kLs ${ISO_URL}.sha512 | awk '{print $1}')
-MOUNT_PATH=/tmp/texlive
+MOUNT_PATH=/media/texlive
 
-# Check aria2c exists
-if [ -n $(which aria2c) ]; then 
-  apt install aria2 -y
-fi
+apt install aria2 kmod -y
 
 # Download texlive ISO
 aria2c -x16 -s16 --file-allocation=none -c -d $(dirname $ISO_PATH) -o $(basename $ISO_PATH) --checksum=md5=$ISO_MD5 --checksum=sha-512=$ISO_SHA512 "$ISO_URL"
 # Mount ISO
 mkdir -p $MOUNT_PATH
-ls /tmp/
-ls /sbin/ /bin/
-/sbin/modprobe loop
+modprobe loop
 mount -o ro,loop,noauto -t iso9660 $ISO_PATH $MOUNT_PATH
 # Install texlive
 echo I | ${MOUNT_PATH}/install-tl -no-gui -lang ja
