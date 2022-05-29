e36096dae6d54f4db80c17445bd54ed0ceb64c7f
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-05-29 20:47:42 +0900

    add gitpod, tried to reduce image size

--- .devcontainer/scripts/install_texlive.sh
+++ .devcontainer/scripts/install_texlive.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-ISO_URL="https://mirror.ctan.org/systems/texlive/Images/texlive2022.iso"
-ISO_PATH=/tmp/texlive.iso
+TEXLIVE_VERSION=2022
+TEXLIVE_ISO_URL="https://mirror.ctan.org/systems/texlive/Images/texlive${TEXLIVE_VERSION}.iso"
+TEXLIVE_ISO_PATH=/tmp/texlive.iso
+TEXLIVE_MOUNT_PATH=/media/texlive
+TEXLIVE_PROFILE_PATH="/tmp/scripts/texlive${TEXLIVE_VERSION}.profile"
+
 echo Getting hashes...
-ISO_MD5=$(curl -kLs ${ISO_URL}.md5 | awk '{print $1}')
-ISO_SHA512=$(curl -kLs ${ISO_URL}.sha512 | awk '{print $1}')
-MOUNT_PATH=/media/texlive
+TEXLIVE_ISO_MD5=$(curl -kLs ${TEXLIVE_ISO_URL}.md5 | awk '{print $1}')
+TEXLIVE_ISO_SHA512=$(curl -kLs ${TEXLIVE_ISO_URL}.sha512 | awk '{print $1}')
 
 apt install aria2 p7zip-full libfontconfig1 libfontconfig-dev libfontconfig1-dev -y
 
 # Download texlive ISO
-aria2c -x16 -s16 --file-allocation=none -c -d $(dirname $ISO_PATH) -o $(basename $ISO_PATH) --checksum=md5=$ISO_MD5 --checksum=sha-512=$ISO_SHA512 "$ISO_URL"
+aria2c -x16 -s16 --file-allocation=none -c -d $(dirname $TEXLIVE_ISO_PATH) -o $(basename $TEXLIVE_ISO_PATH) --checksum=md5=$TEXLIVE_ISO_MD5 --checksum=sha-512=$TEXLIVE_ISO_SHA512 "$TEXLIVE_ISO_URL"
 # Mount ISO
-mkdir -p $MOUNT_PATH
-# mount -o ro,loop,noauto -t iso9660 $ISO_PATH $MOUNT_PATH
-7z x $ISO_PATH -o${MOUNT_PATH}
+mkdir -p $TEXLIVE_MOUNT_PATH
+# mount -o ro,loop,noauto -t iso9660 $TEXLIVE_ISO_PATH $TEXLIVE_MOUNT_PATH
+7z x $TEXLIVE_ISO_PATH -o${TEXLIVE_MOUNT_PATH}
 # Install texlive
-chmod +x ${MOUNT_PATH}/install-tl
-echo I | ${MOUNT_PATH}/install-tl -no-gui -lang ja
+chmod +x ${TEXLIVE_MOUNT_PATH}/install-tl
+echo I | ${MOUNT_PATH}/install-tl -no-gui -lang ja 
 # Unmount ISO
 # umount -r $MOUNT_PATH
 rm -fr ${MOUNT_PATH}
 # Remove ISO
-rm $ISO_PATH
+rm $TEXLIVE_ISO_PATH
 
 # tlmgr setup
 TLMGR_PATH=$(ls --color=none /usr/local/texlive/*/bin/*/tlmgr|tail -n1)
